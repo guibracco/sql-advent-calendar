9150d0f9598d23cfc78058dd03069226d0c33def
Guilherme Bracco <102906034+guibracco@users.noreply.github.com>
2025-12-02 14:43:21 +0100

    formatting

--- 2025/01/01.sql
+++ 2025/01/01.sql
@@ -1,4 +1,3 @@
-
 -- Question 1 of 1
 --  
 -- Every year, the city of Whoville conducts a Reindeer Run to find the best reindeers for Santa's Sleigh. Can you write a query to return the name and rank of the top 7 reindeers in this race?
